8f454a2c447e1584d3abdeefeb7819b678f7d420
geardyy22 <degraciakellyjay@gmail.com>
2024-10-20 23:49:04 +0800

    CRUD API

--- app.js
+++ app.js
@@ -15,9 +15,32 @@ mongoose.connect(databaseURL, { useNewUrlParser: true, useunifiedTopology: true}
 app.use(express.json());
 
 //get data
-app.get('/users', async (req, res) => {
+app.get('/user', async (req, res) => {
         const users = await User.find(); //same as SELECT * FROM
         res.json(users);
+})
+
+//getting data by ID
+app.get('/user/:id', async (req, res) => {
+    const user = await User.findById(req.params.id) //requires parameter id in the URL
+    res.json(user);
+})
+
+//updating a user by ID
+app.put('/user/:id', async (req, res) => {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, 
+        { new: true, runValidators: true});
+    if(!updatedUser) {
+        return res.status(404).json({ message: "Item Not Found" })
+    }
+
+    res.json(updatedUser);
+})
+
+//Deleting User
+app.delete('/user/:id', async (req, res) => {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User is Deleted", deletedUser})
 })
 
 //Creating data
@@ -28,7 +51,7 @@ app.post('/user', async (req, res) => {
         res.status(201).json(newUser); //respond of the created user
 
     }catch (err) {
-        console.log("I hae an error", err)
+        console.log("I have an error", err)
     }
 
 })
